b12784006e3dbd5e90d8e7ddc795a0c69fa093b1
Paul Crook <paul.crook@nrel.gov>
2024-08-29 07:39:46 -0600

    add comment highlighting problematic solution

--- csm/__init__.py
+++ csm/__init__.py
@@ -4,11 +4,11 @@ from csm.csm import CSM
 from csm.run import run_parameter_config
 from csm.util import import_model
 
+__all__ = ["CSM", "run_parameter_config"]
+
 # dynamically import all models in the "model" directory
 # assumes the class name and file name are the same
+# This is not pretty and I don't like it
 parent_dir = Path(__file__).parent / "model"
 for p in parent_dir.glob("[!__]*[!__].py"):
     globals()[p.stem] = import_model(p.stem, parent_dir)
-
-
-__all__ = ["CSM", "run_parameter_config"]
