22807eafa1426157c82d55e0b0728612eaea628b
Paul Crook <paul.crook@nrel.gov>
2024-09-04 16:12:35 -0600

    remove ability to import default models from csm

--- csm/__init__.py
+++ csm/__init__.py
@@ -1,15 +1,5 @@
-from pathlib import Path
-
 from csm.csm import CSM
 from csm.run import run_parameter_config
-from csm.util import import_model
 
 
 __all__ = ["CSM", "run_parameter_config"]
-
-# dynamically import all models in the "model" directory
-# assumes the class name and file name are the same
-# This is not pretty and I don't like it
-parent_dir = Path(__file__).parent / "model"
-for p in parent_dir.glob("[!__]*[!__].py"):
-    globals()[p.stem] = import_model(p.stem, parent_dir)
