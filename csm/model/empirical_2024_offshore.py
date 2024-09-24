15282087c0d9e60940e26f5bf9229eae1b03a2dc
Paul Crook <paul.crook@nrel.gov>
2024-09-24 13:00:59 -0600

    update relationships

--- csm/model/empirical_2024_offshore.py
+++ csm/model/empirical_2024_offshore.py
@@ -1,19 +1,9 @@
 import numpy as np
 
-from csm.model._base import CSMMixin
+from csm.model.empirical_2020 import Empirical2020
 
 
-class Empirical2024Offshore(CSMMixin):
-
-    def rotor_angular_velocity_max(tip_speed_max, rotor_radius):
-        return tip_speed_max / rotor_radius
-
-    def rotor_torque_max(
-        turbine_rating, rotor_efficiency_max, rotor_angular_velocity_max
-    ):
-        return (turbine_rating * 1e6 / rotor_efficiency_max) / (
-            rotor_angular_velocity_max
-        )
+class Empirical2024Offshore(Empirical2020):
 
     def tower_mass(hub_height, water_depth, rotor_diameter):
         x = (hub_height + water_depth) * rotor_diameter**2
@@ -28,14 +18,14 @@ class Empirical2024Offshore(CSMMixin):
     def monopile_mass(water_depth):
         return 579824.0 * np.exp(0.0307 * water_depth)
 
-    def gearbox_mass(rotor_torque_max, is_direct_drive):
-        if is_direct_drive:
-            return 0.0
-        else:
-            return 3487.9 * np.exp(0.0184 * rotor_torque_max / 1e6)
+    def gearbox_mass(rotor_torque_max):
+        return 3487.9 * np.exp(0.0184 * rotor_torque_max / 1e6)
 
-    def gearbox_cost(gearbox_mass, is_direct_drive):
-        if is_direct_drive:
-            return 0.0
-        else:
-            return 94.425 * gearbox_mass - 147064.0
+    def monopile_cost(monopile_mass):
+        return 2.2599 * monopile_mass + 590463.0
+
+    def tower_cost(tower_mass):
+        return 2.4036 * tower_mass + 310177.0
+
+    def gearbox_cost(gearbox_mass):
+        return 94.425 * gearbox_mass - 147064.0
