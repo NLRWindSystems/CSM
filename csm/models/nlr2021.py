edae7f84aff994e26f1813925db905775c007af5
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-08-21 15:13:10 -0700

    add blade mass model

--- csm/models/nlr2021.py
+++ csm/models/nlr2021.py
@@ -1,6 +1,16 @@
+from attrs import define, fields
+
+from csm.models.utils import create_field
+from csm.models.base_model import CSMBase
+
+
+base = fields(CSMBase)
+
+
 from csm.models import Land2020NLR
 
 
+@define
 class Land2021NLR(Land2020NLR):
     # untouched
     # spinner: k	2.3255	b	204.65
@@ -13,11 +23,15 @@ class Land2021NLR(Land2020NLR):
     # platform mainframe: k	0.005
     # crane mass: no change
 
+    blade_mass_coeff = base.blade_mass_coeff.reuse(default=8.3612)
+    blade_mass_coeff2 = base.blade_mass_coeff.reuse(default=-620.03)
+    blade_mass_intercept = create_field(float, "unitless", "input", default=17847)
+
     def __attrs_post_init__(self):
         """Updates the parameter mapping for new mass and cost relationships."""
         self.parameter_map["blade_mass"] = (
             "rotor_diameter",
-            "blade_mass_coeff1",
+            "blade_mass_coeff",
             "blade_mass_coeff2",
             "blade_mass_intercept",
         )
@@ -44,9 +58,31 @@ class Land2021NLR(Land2020NLR):
         )
 
     def calculate_blade_mass(self):
-        # m = a*(RD/2)^2 + b*(RD/2) + c
-        # a	8.3612	b	-620.03	c	17847
-        ...
+        """Calculates and sets :py:attr:`blade_mass` if it was not provided by the user.
+
+        .. math:: k1 * radius^2 + k2 * radius + b
+
+        where:
+
+        - :math:`k1 =` :py:attr:`blade_mass_coeff`
+        - :math:`k2 =` :py:attr:`blade_mass_coeff2`
+        - :math:`radius =` :py:attr:`rotor_diameter` / 2
+        - :math:`b =` :py:attr:`blade_mass_intercept`
+
+        Args:
+            blade_mass_coeff (float): :math:`k1` in the mass equation above.
+            blade_mass_coeff2 (float): :math:`k2` in the mass equation above.
+            rotor_diameter (float): Diameter of the swept area of the turbine blades.
+            blade_mass_intercept (float): :math:`b` in the mass equation above.
+
+        Raises:
+            ValueError: Raised if the required parameters have not been provided or calculated.
+        """
+        self.blade_mass = (
+            self.blade_mass_coeff * self.rotor_radius**2
+            + self.blade_mass_coeff2 * self.rotor_radius
+            + self.blade_mass_intercept
+        )
 
     def calculate_hub_mass(self):
         # m = k*P^b
