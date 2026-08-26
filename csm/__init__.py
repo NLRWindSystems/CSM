3727578f121f191ef17d65c35b278e28765fced1
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-08-26 15:09:25 -0700

    update base model documentation and reference tables for common functionality

--- csm/__init__.py
+++ csm/__init__.py
@@ -1,7 +1,4 @@
-from csm.models.nlr2015 import Land2015NLR
-from csm.models.nlr2020 import Land2020NLR
-from csm.models.nlr2021 import Land2021NLR
-from csm.models.base_model import CSMBase
+from csm.models import CSMBase, Land2015NLR, Land2020NLR, Land2021NLR
 
 
 __all__ = ["CSM", "run_config"]
---
+++ csm/models/__init__.py
@@ -0,0 +1,4 @@
+from csm.models.nlr2015 import Land2015NLR
+from csm.models.nlr2020 import Land2020NLR
+from csm.models.nlr2021 import Land2021NLR
+from csm.models.base_model import CSMBase
