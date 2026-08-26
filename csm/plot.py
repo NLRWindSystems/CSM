15632de168da20ba8642b78bb3bf34cba91d8ed5
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-08-26 08:48:23 -0700

    add empirical/background data plotting capabilities

--- csm/plot.py
+++ csm/plot.py
@@ -3,6 +3,8 @@
 import warnings
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
 
@@ -15,14 +17,17 @@ def plot_mass_cost_comparison(
     component: str,
     parameter_label: str,
     reference_turbines: dict[str, dict] | None = None,
+    background_data: pd.DataFrame | None = None,
     mass_units: str = "tonnes",
     cost_basis: str = "millions",
     save_name: str | Path | None = None,
     fig_kwargs: dict | None = None,
-    scatter_kwargs: dict | None = None,
+    reference_scatter_kwargs: dict | None = None,
+    background_scatter_kwargs: dict | None = None,
     plot_kwargs: dict | None = None,
     axis_label_kwargs: dict | None = None,
     model_plot_settings: dict[str, str] | None = None,
+    turbine_scatter_settings: dict[str, str] | None = None,
     mass_xlim: tuple[float, float] | None = None,
     mass_ylim: tuple[float, float] | None = None,
     cost_xlim: tuple[float, float] | None = None,
@@ -31,14 +36,32 @@ def plot_mass_cost_comparison(
     return_fig_ax: bool = False,
 ) -> None | tuple[plt.Figure, tuple[plt.Axes, plt.Axes]]:
     """Removes the missing docstring error."""
-    # Plot settings handling
+    # Handle None defaults for expected non-None types
     fig_settings = {"dpi": 200, "figsize": (9, 4)}
     if fig_kwargs is not None:
         fig_settings.update(fig_kwargs)
 
-    # scatter_settings = {} if scatter_kwargs is None else scatter_kwargs
-    plot_settings = {} if plot_kwargs is None else plot_kwargs
-    axis_label_settings = {} if axis_label_kwargs is None else axis_label_kwargs
+    reference_scatter_settings = {}
+    if reference_scatter_kwargs is not None:
+        reference_scatter_settings.update(reference_scatter_kwargs)
+
+    background_scatter_settings = {}
+    if background_scatter_kwargs is not None:
+        background_scatter_settings.update(background_scatter_kwargs)
+
+    plot_settings = {}
+    if plot_kwargs is not None:
+        plot_settings.update(plot_kwargs)
+
+    axis_label_settings = {}
+    if axis_label_kwargs is not None:
+        axis_label_settings.update(axis_label_kwargs)
+
+    if turbine_scatter_settings is None:
+        turbine_scatter_settings = {}
+
+    if model_plot_settings is None:
+        model_plot_settings = {}
 
     # Create models and results
     metrics = [f"{component}_mass", f"{component}_cost"]
@@ -100,18 +123,50 @@ def plot_mass_cost_comparison(
         ax.grid()
         ax.set_axisbelow(True)
 
+    # Background scatter points
+    if background_data is not None:
+        scatter_data = background_data.loc[:, [parameter, *metrics]]
+        parameter_values = scatter_data[parameter].to_numpy()
+        mass = scatter_data[metrics[0]].to_numpy() * mass_scale
+        cost = scatter_data[metrics[1]].to_numpy() * cost_scale
+        ax1.scatter(
+            parameter_values, mass, **background_scatter_settings, label="Individual Turbines"
+        )
+        ax2.scatter(mass, cost, **background_scatter_settings)
+
     # Model line plots
     for name, _results in results.items():
         parameter_values = _results.columns
         mass = _results.loc[f"{component}_mass"] * mass_scale
         cost = _results.loc[f"{component}_cost"] * cost_scale
 
-        formatting = model_plot_settings.get(name, {})
-        ax1.plot(parameter_values, mass, label=name, **formatting, **plot_settings)
-        ax2.plot(mass, cost, **plot_settings, **formatting, **plot_settings)
+        formatting = plot_settings | model_plot_settings.get(name, {})
+        ax1.plot(parameter_values, mass, label=name, **formatting)
+        ax2.plot(mass, cost, **plot_settings, **formatting)
 
     # Reference turbine scatter plots
     # TODO: reference turbine scatter plots
+    if reference_turbines is None:
+        reference_turbines = {}
+    for turbine, config in reference_turbines.items():
+        if (parameter_value := config.get(parameter)) is None:
+            continue
+        turbine_models = {
+            name: model.from_dict(config, partial=True) for name, model in models.items()
+        }
+        [model.calculate(*metrics) for model in turbine_models.values()]
+
+        parameter_values = [parameter_value] * len(turbine_models)
+        mass = [getattr(model, f"{component}_mass") for model in turbine_models.values()]
+        cost = [getattr(model, f"{component}_cost") for model in turbine_models.values()]
+        mass = np.array(mass) * mass_scale
+        cost = np.array(cost) * cost_scale
+
+        formatting = reference_scatter_settings | turbine_scatter_settings.get(turbine, {})
+        ax1.scatter(parameter_values, mass, label=turbine, **formatting)
+        ax2.scatter(mass, cost, **formatting)
+
+    # TODO: background data scatter for industry data points
 
     # Post-plot figure handling
     ax1.legend()
